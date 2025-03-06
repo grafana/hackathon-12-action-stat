b84d75d58cbc76174c67a085321b0a2e15989a12
Michelle Erickson <mericks@users.noreply.github.com>
2025-03-06 09:29:42 -0800

    Update doc string, address linter warnings

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -23,20 +23,20 @@ ALLOY_PID=$!
 
 # Set timeout duration (5 minutes = 300 seconds)
 UPLOAD_TIMEOUT=${UPLOAD_TIMEOUT:-300}
-START_TIME=$SECONDS
+START_TIME=${SECONDS}
 
-# Watch for workflow_logs.json to be deleted or timeout
+# Watch for file contents of log direcotry to be deleted or timeout
 while true; do
   # Check if timeout reached
-  if [ $((SECONDS - START_TIME)) -ge "${UPLOAD_TIMEOUT}" ]; then
+  if [[ $((SECONDS - START_TIME)) -ge "${UPLOAD_TIMEOUT}" ]]; then
     echo "Timeout reached after ${UPLOAD_TIMEOUT} seconds. Killing Alloy process."
-    kill $ALLOY_PID || true
-    exit 1
+    kill "${ALLOY_PID}" || true
+    return 1
   fi
 
   # Check if log directory is empty
-  if [ -z "$(ls -A "${LOG_DIRECTORY}")" ]; then
-    kill $ALLOY_PID || true
+  if [[ -z "$(ls -A "${LOG_DIRECTORY}")" ]]; then
+    kill "${ALLOY_PID}" || true
     break
   fi
 done
